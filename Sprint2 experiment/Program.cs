7d56c810a5076e446d1324315d8ff8d3c0c9504d
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-11-18 17:59:45 +0100

    Create Program.cs

---
+++ Sprint2 experiment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sprint2_experiment
+{
+    class Program
+    {
+        Program()
+        {
+            Window fenster = new Window();
+            fenster.Title = "MeinFenster";
+
+            UserControl1 myGUI = new UserControl1();
+            fenster.Content = myGUI;
+            fenster.ShowDialog();
+
+
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
+
