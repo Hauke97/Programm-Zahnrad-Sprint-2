7558a02e7de1f7c71e2c89d5d8dccd9146ba712a
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-11-18 17:59:55 +0100

    Create UserControl1.xaml.cs

---
+++ Sprint2 experiment/UserControl1.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sprint2_experiment
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+  
+
+        private void btn_berechnen_Click(object sender, RoutedEventArgs e)
+        {
+            double m = Convert.ToDouble(tb_modul.Text);
+            double z = Convert.ToDouble(tb_zähnezahl.Text);
+            double d = Convert.ToDouble(tb_teilkreisdurchmesser1.Text);
+
+
+
+            
+
+            if (m != 0 && z != 0)
+            {
+                double p = Math.PI / m;
+                d = m * z;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+
+            }
+
+
+            else if (m != 0 && d != 0)
+            {
+                z = d / m;
+                double p = Math.PI / m;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+
+                tb_teilung.Text = Convert.ToString(p);
+            }
+
+            else if (z != 0 && d != 0)
+            {
+                m = d / z;
+                double p = Math.PI / m;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+                
+            }
+
+        }
+       
+    }
+}
