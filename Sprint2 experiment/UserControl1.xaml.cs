4bb0557fb8de61b6f6d23f74587ff2ca93786fef
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-11-23 19:44:26 +0100

    Update UserControl1.xaml.cs

--- Sprint2 experiment/UserControl1.xaml.cs
+++ Sprint2 experiment/UserControl1.xaml.cs
@@ -40,31 +40,71 @@ namespace Sprint2_experiment
         {
             double m = Convert.ToDouble(tb_modul.Text);
             double z = Convert.ToDouble(tb_zähnezahl.Text);
-            double d = Convert.ToDouble(tb_teilkreisdurchmesser1.Text);
-
-            m = d / z;
-            z = d / m;
-            double p = Math.PI * m;
-            d = m * z;
-            double c = 0.167 * m;
-            double df = d - 2 * (m + c);
-            double da = 2 * m + d;
-            double h = 2 * m + c;
-            double hf = m + c;
-            double ha = m;
-
-            tb_teilung.Text = Convert.ToString(p);
-            tb_kopfspiel.Text = Convert.ToString(c);
-            tb_fußkreisdurchmesser.Text = Convert.ToString(df);
-            tb_kopfkreisdurchmesser.Text = Convert.ToString(da);
-            tb_zahnhöhe.Text = Convert.ToString(h);
-            tb_zahnfußhöhe.Text = Convert.ToString(hf);
-            tb_zahnkopfhöhe.Text = Convert.ToString(ha);
-            
+            double d = Convert.ToDouble(tb_teilkreisdurchmesser.Text);
 
 
 
-           
+
+
+            if (m != 0 && z != 0)
+            {
+                double p = Math.PI / m;
+                d = m * z;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+                tb_fußkreisdurchmesser.Text = Convert.ToString(df);
+                tb_kopfkreisdurchmesser.Text = Convert.ToString(da);
+                tb_zahnhöhe.Text = Convert.ToString(h);
+                tb_zahnfußhöhe.Text = Convert.ToString(hf);
+                tb_zahnkopfhöhe.Text = Convert.ToString(ha);
+                tb_teilung.Text = Convert.ToString(p);
+                tb_kopfspiel.Text = Convert.ToString(c);
+            }
+
+
+            else if (m != 0 && d != 0)
+            {
+                z = d / m;
+                double p = Math.PI / m;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+
+                tb_fußkreisdurchmesser.Text = Convert.ToString(df);
+                tb_kopfkreisdurchmesser.Text = Convert.ToString(da);
+                tb_zahnfußhöhe.Text = Convert.ToString(h);
+                tb_zahnfußhöhe.Text = Convert.ToString(hf);
+                tb_zahnkopfhöhe.Text = Convert.ToString(ha);
+                tb_teilung.Text = Convert.ToString(p);
+                tb_kopfspiel.Text = Convert.ToString(c);
+            }
+
+            else if (z != 0 && d != 0)
+            {
+                m = d / z;
+                double p = Math.PI / m;
+                double c = 0.167 * m;
+                double df = d - 2 * (m + c);
+                double da = d + 2 * m;
+                double h = 2 * m + c;
+                double hf = m + c;
+                double ha = m;
+
+                tb_fußkreisdurchmesser.Text = Convert.ToString(df);
+                tb_kopfkreisdurchmesser.Text = Convert.ToString(da);
+                tb_zahnfußhöhe.Text = Convert.ToString(h);
+                tb_zahnfußhöhe.Text = Convert.ToString(hf);
+                tb_zahnkopfhöhe.Text = Convert.ToString(ha);
+                tb_teilung.Text = Convert.ToString(p);
+                tb_kopfspiel.Text = Convert.ToString(c);
+            }
 
         }
 
