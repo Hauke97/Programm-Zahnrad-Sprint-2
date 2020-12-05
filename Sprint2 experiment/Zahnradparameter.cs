7d0c2bb8706bf97fa57f546cfa5e801d3b0d81d7
a-rondorf <72039786+a-rondorf@users.noreply.github.com>
2020-12-05 13:18:17 +0100

    Create Zahnradparameter.cs

---
+++ Sprint2 experiment/Zahnradparameter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sprint2_experiment
+{
+    internal class Zahnradparameter
+    {
+        public double p;
+        public double c;
+        public double df;
+        public double da;
+        public double h;
+        public double hf;
+        public double ha;
+        public double a;
+        public double db;
+
+
+        public Zahnradparameter()
+
+        {
+        }
+
+        public void Berechne_Parameter(double m, double z, double d)
+        {
+             p = Math.PI / m;
+             c = 0.167 * m;
+             df = d - 2 * (m + c);
+             da = d + 2 * m;
+             h = 2 * m + c;
+             hf = m + c;
+             ha = m;
+             a = 20;
+             db = m * z + Math.Cos(a);
+
+        }
+    }
+}
